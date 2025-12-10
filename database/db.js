bec86fac58d91bb3ab834f9b7b50b5973dc52771
Pedro Antônio Heinrich <pedroantonioheinrich@gmail.com>
2025-12-10 01:31:34 -0300

    rollback

---
+++ database/db.js
@@ -0,0 +1,31 @@
+class Task {
+    static tasks = [];
+
+    static async addNewTask(taskObj) {
+        return this.tasks.push(taskObj)
+    }
+
+    static taskCount() {
+        return this.tasks.length;
+    }
+
+    static seeAllTasks() {
+        return this.tasks;
+    }
+
+    static getTaskById(id) {
+        return this.tasks.find(e => e.id === id) || "Task ID not found!!";
+    }
+
+    static removeAll() {
+        this.tasks = [];
+        return this.tasks;
+    }
+
+    static removeTaskById(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+        return this.tasks;
+    }
+}
+
+export default Task;
